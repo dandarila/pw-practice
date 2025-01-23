0576d0e305b758dcbb1e56fc7295362cd060db84
dandarila <marian_dandarila@yahoo.com>
2025-01-23 09:30:10 +0200

    capture response

--- tests/Network.spec.js
+++ tests/Network.spec.js
@@ -4,6 +4,7 @@ import APiUtils from '../utils/APiUtils';
 let response;
 const loginPayload = { userEmail: 'marian@mailinator.com', userPassword: 'Password123' };
 const orderPayload = { orders: [{country: 'India', productOrderedId: '676a6619e2b5443b1f004fff'}]};
+const fakePayloadOrders = {data: [], message: "No Orders"}
 
 test.beforeAll( async() => {
   const apiContext =  await request.newContext();
@@ -20,19 +21,23 @@ test('Client app login', async ({page}) => {
     await page.addInitScript(value => {window.localStorage.setItem('token', value)}, response.token);
     await page.goto('https://rahulshettyacademy.com/client/');
     
+    await page.route('https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/674de029eb3c71ba7921de78', async route => {
+        //intercepting response - API response -> send to browser to render it on FE
+        const response = await page.request.fetch(route.request());
+        let body = fakePayloadOrders;
+        console.log(response);
+        route.fulfill({
+            response, 
+            body,
+        });
+    });
+    
     const storedToken = await page.evaluate(() => window.localStorage.getItem('token'));
     console.log('Stored Token:', storedToken);
     
     await page.locator('[routerlink *= "/dashboard/myorders"]').first().click();
-    await page.locator('tbody').waitFor();
-    const rows = page.locator('tbody tr');
+    // await page.locator('tbody').waitFor();
+    // const rows = page.locator('tbody tr');
 
-    for (let i=0; i < await rows.count(); i++) {
-        const rowOrderId = await rows.nth(i).locator('th').textContent();
-        if (response.orderId.includes(rowOrderId)) {
-            await rows.nth(i).locator('button').first().click();
-            break;
-        }
-    }
-    expect (response.orderId.includes(await page.locator('.col-text').textContent())).toBeTruthy();
+    
 });
